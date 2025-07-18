f3ece5a735deaf103de5c089f8370371be8ebc8f
krebso <63379127+thekrebso@users.noreply.github.com>
2025-07-18 23:02:29 +0200

    chess piece type enum

---
+++ src/ChessPiece.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Piece:
+    class Type(Enum):
+        EMPTY = 0
+        PAWN = 1
+        KNIGHT = 2
+        BISHOP = 3
+        ROOK = 4
+        QUEEN = 5
+        KING = 6
